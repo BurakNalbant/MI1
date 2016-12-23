4a30cd62d32d1a9a285432e96b5d78b170b7966d
BurakNalbant <burak.nalbant@student.odisee.be>
2016-12-23 21:32:09 +0100

    Add files via upload

---
+++ MI1/Logboek/Aryan_Shop/formulier.php
@@ -0,0 +1,11 @@
+<?php
+ 
+  $to = "yourname@yourwebsite.com";
+ 
+  $headers = "From: $email_from \r\n";
+ 
+  $headers .= "Reply-To: $visitor_email \r\n";
+ 
+  mail($to,$email_subject,$email_body,$headers);
+ 
+ ?>
